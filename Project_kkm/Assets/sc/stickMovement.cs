aa41e4ad120f4ac60819a76640f1e78da0ab41d1
jackykkk <41138304+jackykwan@users.noreply.github.com>
2018-07-13 12:39:02 +0800

    updated

---
+++ Project_kkm/Assets/sc/stickMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position += new Vector3(0, speed*Time.deltaTime, 0);
+
+	}
+}
